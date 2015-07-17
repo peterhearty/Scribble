31ffe4a412439b5d700861c4ff4c470dbc08b56a
Peter Hearty <pete@hearty.plus.com>
2015-07-17 08:34:03 +0100

    Implement draw item selection as a popup menu

--- app/src/main/java/uk/org/platitudes/scribble/buttonhandler/DrawToolButtonHandler.java
+++ app/src/main/java/uk/org/platitudes/scribble/buttonhandler/DrawToolButtonHandler.java
@@ -3,24 +3,20 @@
  */
 package uk.org.platitudes.scribble.buttonhandler;
 
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 
+import uk.org.platitudes.scribble.R;
 import uk.org.platitudes.scribble.ScribbleView;
 import uk.org.platitudes.scribble.drawitem.DrawItem;
 import uk.org.platitudes.scribble.drawitem.FreehandDrawItem;
 import uk.org.platitudes.scribble.drawitem.ScrollItem;
 
-public class DrawToolButtonHandler implements View.OnClickListener {
-
-    public static int FREE = 0;
-    public static int LINE = 1;
-    public static int TEXT = 2;
-    public static int SELECT = 3;
-    public static int SCROLL = 4;
-
-    public static String[] buttonTexts = {"Free", "Line", "Text", "select", "scroll"} ;
+public class DrawToolButtonHandler implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
     private Button mDrawToolButton;
 
@@ -32,27 +28,33 @@ public class DrawToolButtonHandler implements View.OnClickListener {
     public void onClick(View v) {
         Button b = (Button) v;
         CharSequence curText = b.getText();
+        createMenu();
+    }
 
-        for (int i=0; i<buttonTexts.length; i++) {
-            if (curText.equals(buttonTexts[i])) {
-                int j = i+1;
-                if (j == buttonTexts.length) j=0;
-                    b.setText(buttonTexts[j]);
-                break;
-            }
-        }
+    private void createMenu () {
+        PopupMenu popup = new PopupMenu(mDrawToolButton.getContext(), mDrawToolButton);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.draw_button_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(this);
+        popup.show();
     }
 
     public DrawItem generateDrawItem (MotionEvent event, ScribbleView scribbleView) {
         if (mDrawToolButton == null) return null;
 
         DrawItem result = null;
-        if (mDrawToolButton.getText().equals(buttonTexts[FREE])) {
+        if (mDrawToolButton.getText().equals("free")) {
             result = new FreehandDrawItem(event, scribbleView);
-        } else if (mDrawToolButton.getText().equals(buttonTexts[SCROLL])) {
+        } else if (mDrawToolButton.getText().equals("scroll")) {
             result = new ScrollItem(event, scribbleView);
         }
         return result;
     }
 
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        CharSequence menuTitle = item.getTitle();
+        mDrawToolButton.setText(menuTitle);
+        return true;
+    }
 }
