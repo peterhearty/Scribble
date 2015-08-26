e9afbe2e541c2f1a75518168cbe9b86ff98ededc
Peter Hearty <pete@hearty.plus.com>
2015-08-26 16:46:27 +0100

    Made start on Roboelectric for unit testing

--- app/src/test/java/Tester.java
+++ app/src/test/java/Tester.java
@@ -2,12 +2,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import drawItem.FloatAndDeltasTest;
+import drawItem.FreehandTest;
 import drawItem.HandleTest;
 import drawItem.LineTest;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        FreehandTest.class,
         FloatAndDeltasTest.class,
         HandleTest.class,
         LineTest.class
---
+++ app/src/test/java/drawItem/FreehandTest.java
@@ -0,0 +1,88 @@
+/**
+ * This source code is not owned by anybody. You can can do what you like with it.
+ */
+package drawItem;
+
+import android.os.Build;
+import android.view.MotionEvent;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import mockClasses.TestCanvas;
+import uk.org.platitudes.scribble.BuildConfig;
+import uk.org.platitudes.scribble.ScribbleMainActivity;
+import uk.org.platitudes.scribble.ScribbleView;
+import uk.org.platitudes.scribble.drawitem.freehand.FreehandCompressedDrawItem;
+
+/**
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.JELLY_BEAN, constants = BuildConfig.class)
+public class FreehandTest extends TestCase{
+
+    private float[] x1s = {0,10,20,30,20,10,0};
+    private float[] y1s = {0,10,20,30,40,50,60};
+
+    private ScribbleView scribbleView;
+    private ScribbleMainActivity activity;
+    private MotionEvent motionEvent;
+    private TestCanvas canvas;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        activity = Robolectric.buildActivity(ScribbleMainActivity.class).create().get();
+//activity = new ScribbleMainActivity();
+        scribbleView = activity.getmMainView();
+        motionEvent = MotionEvent.obtain(0L, 0L, MotionEvent.ACTION_DOWN, 10f, 10f, 0);
+        canvas = new TestCanvas();
+    }
+
+    private void resetEverything() {
+        canvas.testReset();
+        motionEvent.setLocation(10f, 10f);
+        scribbleView.setmScrollOffset(0, 0);
+        activity.getmZoomButtonHandler().setsZoom(1);
+    }
+
+    private FreehandCompressedDrawItem createFreehand (float[] xs, float[] ys) {
+        motionEvent.setLocation(xs[0], ys[0]);
+        FreehandCompressedDrawItem free = new FreehandCompressedDrawItem(motionEvent, scribbleView);
+        for (int i=1; i<xs.length; i++) {
+            motionEvent.setLocation(xs[i], ys[i]);
+            free.handleMoveEvent(motionEvent);
+        }
+        return free;
+    }
+
+    private boolean testCanvasPositions (float[] xs, float[] ys) {
+        boolean result = true;
+        for (int i=0; i<xs.length-1; i++) {
+            if (!canvas.testStartPosition(i, xs[i], ys[i])) {
+                result = false;
+                break;
+            }
+            if (!canvas.testEndPosition(i, xs[i+1], ys[i+1])) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Test
+    public void simpleFreehandTest () {
+        FreehandCompressedDrawItem free = createFreehand(x1s, y1s);
+        free.draw(canvas);
+        assertTrue(canvas.testDrawCount(x1s.length-1));
+        assertTrue(testCanvasPositions(x1s, y1s));
+    }
+}
